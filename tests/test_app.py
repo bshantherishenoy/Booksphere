a85edbb5c38e4de3b4ebdd454fb48433fb0d88e6
bshantherishenoy <bshantherishenoy777@gmail.com>
2026-05-28 18:49:34 +0530

    changes to the test

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,14 +6,3 @@ def test_home():
     response = client.get("/")
 
     assert response.status_code == 200
-
-
-def test_get_books():
-    client = app.test_client()
-    response = client.get("/books")
-
-    assert response.status_code == 200
-    assert response.json == [
-        {"id": 1, "name": "Atomic Habits"},
-        {"id": 2, "name": "Deep Work"}
-    ]
